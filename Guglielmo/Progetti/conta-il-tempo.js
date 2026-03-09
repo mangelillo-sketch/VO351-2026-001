1508eb17f758fbb14567675661c85cd9677dab6c
Guglielmo <guje97@gmail.com>
2026-03-09 17:50:15 +0100

    Esercizio per casa
    Completamento(spero) dell'esercizio assegnato il 02/03/2026

--- Guglielmo/Progetti/conta-il-tempo.js
+++
@@ -1,6 +0,0 @@
-// Dati un numero di secondi calcolo la quantità di ore, minuti e secondi corrispondenti //
-
-var secondi = 23490;
-var minuti = secondi/60;
-var ore = secondi/3600;
-console.log(ore, minuti, secondi);
