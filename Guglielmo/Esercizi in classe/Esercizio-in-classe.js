47d3e9d727a36cb7f2dd3e7fa518b47509780f76
Guglielmo <guje97@gmail.com>
2026-07-21 20:30:49 +0200

    Update

--- Guglielmo/Esercizi in classe/Esercizio-in-classe.js
+++ Guglielmo/Esercizi in classe/Esercizio-in-classe.js
@@ -1,22 +1,30 @@
-// CREARE E GESTIRE UNA RUBRICA TELEFONICA CON ANNESSE FUNZIONI 
+// CREARE E GESTIRE UNA RUBRICA TELEFONICA CON ANNESSE FUNZIONI
 
 let rubrica = {
-    contatti: [],
-    aggiungiContatto: function(nome, cognome, telefono){
-        let contatto = {
-            nome: nome,
-            cognome: cognome,
-            telefono: telefono
-        };
-        this.contatti.push(contatto);
-    },
-    mostraContatti: function(){
-        for(let i=0; i<this.contatti.length; i++){
-            console.log(this.contatti[i]);
-        }
-    },
+  contatti: [],
+  aggiungiContatto: function (nome, cognome, telefono) {
+    let contatto = {
+      nome: nome,
+      cognome: cognome,
+      telefono: telefono,
+    };
+    this.contatti.push(contatto);
+  },
+  mostraContatti: function () {
+    for (let i = 0; i < this.contatti.length; i++) {
+      console.log(this.contatti[i]);
+    }
+  },
+  cercaContatto: function (nome) {
+    for (let i = 0; i < this.contatti.length; i++) {
+      if (this.contatti[i].nome === nome) {
+        console.log(this.contatti[i]);
+      }
+    }
+  },
 };
 
 rubrica.aggiungiContatto("Mario", "Rossi", "3394638741");
 rubrica.aggiungiContatto("Luca", "Bianchi", "3478923008");
 rubrica.mostraContatti();
+rubrica.cercaContatto("Mario");
